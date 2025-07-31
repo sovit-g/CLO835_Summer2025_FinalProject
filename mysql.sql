77bda744d8df7c00ef153a4e85ad9be59afc9a50
EC2 Default User <ec2-user@ip-172-31-19-153.ec2.internal>
2025-07-31 02:40:33 +0000

    modified mysql.sql and other files to test action workflow

--- mysql.sql
+++ mysql.sql
@@ -9,6 +9,7 @@ primary_skill VARCHAR(20),
 location VARCHAR(20));
 
 INSERT INTO employee VALUES ('1','Amanda','Williams','Smile','local');
-INSERT INTO employee VALUES ('1','Alan','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('2','Alan','Williams','Empathy','alien');
+INSERT INTO employee VALUES ('3','Sovit','Ghimire','Student','scarborough');
 SELECT * FROM employee;
 
